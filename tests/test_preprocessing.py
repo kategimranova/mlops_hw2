f27fdfa819448ee7751fad125f34668df2998177
kategimranova <kate.gimranova@mail.ru>
2022-12-21 18:30:16 +0300

    Update test_preprocessing.py

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -9,5 +9,3 @@ def test_feature_select():
     data = pd.read_csv(path + "/data/train.csv", index_col='PassengerId')
     features = feature_selection_and_preprocessing(data)
     assert features.shape[1] == 6
-
-test_feature_select()
